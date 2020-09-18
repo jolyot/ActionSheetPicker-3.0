15451600b2fbfc548602246771533bbb3b91f945
Kazunori Takaishi <jolyot@gmail.com>
2020-09-19 02:40:05 +0900

    Update Package.swift
    fix syntax error.
    fix target name.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "ActionSheetPicker-3.0",
-                      targets: [.target(name: "Alamofire",
-                                        path: "Source")],
-
     products: [
         .library(
             name: "ActionSheetPicker-3.0",
-            targets: ["ActionSheetPicker-3.0"]),
+            targets: ["ActionSheetPicker-3.0"])
     ],
     targets: [
         .target(
-            name: "CoreActionSheetPicker",
+            name: "ActionSheetPicker-3.0",
             path: "CoreActionSheetPicker")
         ],
     swiftLanguageVersions: [.v5]
